c37f0c840f3fc347e6b04c6e6c6a980aab869d6a
Ishidha L <ishidhaishi@gmail.com>
2026-07-19 09:32:03 +0530

    Added 1Darray.exe

---
+++ src/1Darray.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n,i;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    int arr[n],sum=0;
+    printf("Enter the list of numbers:",n);
+    for(i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+    sum=sum+arr[i];
+
+}
+    for(i=0;i<n;i++){
+        sum=sum-arr[i];
+        arr[i]=sum;
+        printf("%d ",arr[i]);
+    }
+}
