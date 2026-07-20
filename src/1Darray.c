e8a9cd89be4ffc8c19bd1eb5244e03deae6afa5b
Ishidha L <ishidhaishi@gmail.com>
2026-07-20 20:43:08 +0530

    Added pointer.c

--- src/1Darray.c
+++ src/1Darray.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 int main(){
     int n,i;
-    printf("Enter a number:");
     scanf("%d",&n);
     int arr[n],sum=0;
-    printf("Enter the list of numbers:",n);
     for(i=0;i<n;i++){
     scanf("%d",&arr[i]);
     sum=sum+arr[i];
